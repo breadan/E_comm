d7b43cc51b5b1a8a63a0741cbf72aa803b722533
breadan <breadan2020@gmail.com>
2024-07-12 02:00:30 +0300

    EndCategory

--- src/modules/Category/category.routes.js
+++ src/modules/Category/category.routes.js
@@ -1,29 +1,32 @@
-
-import { Router } from "express";
+import { Router } from 'express';
 const router = Router();
-import * as categoryController from './category.contoller.js'
-import expressAsyncHandler from "express-async-handler";
-import { multerMiddleHost } from "../../middlewares/multer.js";
-import { endPointsRoles } from "./category.endpoints.js";
-import { auth } from "../../middlewares/auth.middleware.js";
-import { allowedExtensions } from "../../utils/allowed-extensions.js";
+import * as categoryController from './category.controller.js';
+import expressAsyncHandler from 'express-async-handler';
+import { multerMiddleHost } from '../../middlewares/multer.js';
+import { endPointsRoles } from './category.endpoints.js';
+import { auth } from '../../middlewares/auth.middleware.js';
+import { allowedExtensions } from '../../utils/allowed-extensions.js';
 
+router.post(
+  '/',
+  auth(endPointsRoles.ADD_CATEGORY),
+  multerMiddleHost({
+    extensions: allowedExtensions.image,
+  }).single('image'),
+  expressAsyncHandler(categoryController.addCategory)
+);
 
-router.post('/',
-    auth(endPointsRoles.ADD_CATEGORY),
-    multerMiddleHost({
-        extensions: allowedExtensions.image
-    }).single('image'),
-    expressAsyncHandler(categoryController.addCategory))
+router.put(
+  '/:categoryId',
+  auth(endPointsRoles.ADD_CATEGORY),
+  multerMiddleHost({
+    extensions: allowedExtensions.image,
+  }).single('image'),
+  expressAsyncHandler(categoryController.updateCategory)
+);
 
-router.put('/:categoryId',
-    auth(endPointsRoles.ADD_CATEGORY),
-    multerMiddleHost({
-        extensions: allowedExtensions.image
-    }).single('image'),
-    expressAsyncHandler(categoryController.updateCategory))
-
-
-
-router.get('/', expressAsyncHandler(categoryController.getAllCategories))
+router.get(
+  '/getCategories',
+  expressAsyncHandler(categoryController.getAllCategories)
+);
 export default router;
